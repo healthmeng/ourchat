287723d568b3cdf3cbbbff693191f931374f2c0b
healthmeng <24625527@qq.com>
2017-01-11 17:35:51 +0800

    to be continue

--- src/server/main.go
+++ src/server/main.go
@@ -8,6 +8,7 @@ import(
 "time"
 "dbop"
 "container/list"
+"time"
 "sync"
 //"encoding/json"
 )
@@ -21,21 +22,90 @@ type Message struct{
 	Msgtime string
 }
 
+func (msg* Message)SendOK(){
+}
+
+
 type OLUser struct{
 	*dbop.UserInfo
 	Ctrloff chan int
-	Ctrlmsg chan int
-	SendQ *list.List // type Message
-	Sendlock sync.Mutex
-	PostQ *list.List
-	PostLock sync.Mutex
+	Statmsg chan int
+	Newjob	chan string 
+	SendQ *list.List // type Message，load from db
+	Sendlock *sync.Mutex
+	NetConn net.Conn
 }
 
+
+
 var online_user map[string] *OLUser
+var maplock sync.RWMutex
+
+func (ouser* OLUser)LoadMessageQ(){
+	// called only in writeproc
+}
+
+func (ouser* OLUser)ReadProc(){
+}
+
+func (ouser* OLUser)WriteProc(){
+	for{
+		select{
+			case job:=<-ouser.Newjob:
+			case time.After(timer.Second*
+		}
+	}
+}
+
+func doClose(conn net.Conn, pClose *bool){
+	if *pClose{
+		conn.Close()
+	}
+}
+
+func DoOnline(uinfo* dbop.UserInfo, conn net.Conn){
+	oluser:= &OLUser{dbopUser,
+					make(chan, int), make (chan int), make (chan string),
+					list.New(), new (sync.Mutex),conn}
+/*
+		1. Create ouser obj in map
+		2. Monitor ctrl chan
+		3. Try to Send messages to client and enter communication mode with client(read/write)
+*/
+	maplock.Lock()
+	online_user[userinfo.Username]=oluser
+	maplock.Unlock()
+	go  oluser.ReadProc
+	go  oluser.WriteProc
+	for{
+		select{
+		case ctl:= <-oluser.Ctrloff:
+			// do offline
+		case msg:=<-oluser.Statmsg:
+/* 	1. get send response
+	2. get heart beat
+	3. get client's new message
+	4. get offline inform
+	*/
+			select msg{
+				case 1:
+
+				case 2: // heartbeat
+				default:
+			}
+		case <-time.After(time.Second*60):
+			// no message in 60 seconds, timeout
+			// do offline
+		}
+	}
+}
 
 func procConn(conn net.Conn){
 	conn.SetDeadline(time.Now().Add(time.Second*30))
-	defer conn.Close()
+//	pClose:=new(bool)
+//	*pClose=true
+//	defer doClose(conn,pClose)
+	conn.Close()
 	rd:=bufio.NewReader(conn)
 	command,_,err:=rd.ReadLine()
 	if err!=nil{
@@ -75,10 +145,39 @@ func procConn(conn net.Conn){
 		OK\n | ERROR:err\n(close)
 		go routine (read,write,chan)
 	*/
-		/*	buf,_,err:=rd.ReadLine()
+			usr,_,err:=rd.ReadLine()
 			if err!=nil{
-				log.Println("Read login info error:",err)
-			}*/
+				log.Println("Login read username error:",err)
+				return
+			}
+			psw,_,err:=rd.ReadLine()
+			if err!=nil{
+				log.Println("Login read password error:",err)
+				return
+			}
+			maplock.RLock()
+			online,ok:=online_user[user.Username]
+			maplock.RUnlock()
+			if ok{
+				online.Ctrloff<-1 // start offline
+				<-online.Ctrloff // finished 
+			//	delete(online_user,user.Username) //should be done in connection routine
+			}
+
+			uinfo,_:=dbop.FindUser(string(usr))
+			if uinfo==nil{
+				conn.Write([]byte("ERROR: No such user\n"))
+				return
+			}
+			if uinfo.Password!=psw{
+				conn.Write([]byte("ERROR: Bad user/passwd\n"))
+				return
+			}
+			conn.Write([]byte("OK\n"))
+		//	*pClose=false
+			DoOnline(uinfo,conn)
+
+
 		case "DelUser":
 /*			buf,_,err:=rd.ReadLine()
 			if err!=nil{
@@ -106,7 +205,10 @@ func procConn(conn net.Conn){
 			}
 
 
-			if online,ok:=online_user[user.Username];ok{
+			maplock.RLock()
+			online,ok:=online_user[user.Username]
+			maplock.RUnlock()
+			if ok{
 				online.Ctrloff<-1 // start offline
 				<-online.Ctrloff // finished 
 			//	delete(online_user,user.Username) //should be done in connection routine
