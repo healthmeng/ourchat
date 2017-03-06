c94d4cb4215139388f1f1d33cb0e468fc34d3b1b
tester <24625527@qq.com>
2017-03-06 16:55:00 +0800

    modified make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 export GOPATH=`pwd`
 go get github.com/Go-SQL-Driver/MySQL 
-go build -gcflags "-N" cserver 
-#go install cserver 
+#go build -gcflags "-N" cserver 
+go install cserver 
 go build -gcflags "-N" goclient
